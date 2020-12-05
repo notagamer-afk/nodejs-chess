f6cbb60fd02b28b360119a58a37cba06d0e665da
notagamer-afk <66183902+notagamer-afk@users.noreply.github.com>
2020-12-05 21:23:43 +0200

    Update config.js

--- config.js
+++ config.js
@@ -48,7 +48,7 @@ var html = {
 
 var env = Object.assign({
   NODE_ENV: 'development',
-  API_URL: '//chessapp-env.eba-sjxff4mb.us-east-2.elasticbeanstalk.com:8081'
+  API_URL: '//nodejs-chess-s3.s3-website.us-east-2.amazonaws.com'
 }, process.env);
 
 module.exports = {
